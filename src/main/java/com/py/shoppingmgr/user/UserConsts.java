c4eef22488dd3ed6266151231f26106ddad8c889
pengin00 <pengin@139.com>
2018-09-15 13:57:09 +0800

    user

---
+++ src/main/java/com/py/shoppingmgr/user/UserConsts.java
@@ -0,0 +1,15 @@
+package com.py.shoppingmgr.user;
+
+/**
+ * Created by pengying on 2018/9/15.
+ */
+public class UserConsts {
+    //卖家用户sellerUser, 买家用户 buyerUser
+    public enum UserType{
+        seller,
+        buyer;
+    }
+
+
+
+}
